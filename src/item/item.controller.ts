9d1e3fb050bf54cfd709b3c4a9eca423357a9a4e
Janio <carvalho02011998@gmail.com>
2021-04-16 15:34:21 -0300

    add controller, service and module of item

---
+++ src/item/item.controller.ts
@@ -0,0 +1,14 @@
+// item.controller.ts
+
+import { Controller, Get } from '@nestjs/common';
+import { ItemService } from './item.service';
+
+@Controller('item')
+export class ItemController {
+  constructor(private serv: ItemService) { }
+
+  @Get()
+  public async getAll() {
+    return await this.serv.getAll();
+  }
+}
---
+++ src/scripts/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+set -x
+if [ "$RUN_MIGRATIONS" ]; then
+  echo "RUNNING MIGRATIONS";
+  npm run typeorm:migration:run
+fi
+echo "START SERVER";
